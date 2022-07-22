b47d1f328caac53dd5c72fe0634eccf5d1d6220f
vinaygoswami321 <vinaygoswami2001@gmail.com>
2022-07-22 16:05:46 +0530

    Dynamic data corrected

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ from ElectionData import fetchDataFromWebsite
 from Database import dataFiltering 
 
 data = fetchDataFromWebsite()
+print(data)
 dataFiltering.filterData(data)
 
 # candidate {'Name': 'KAKINADA VENKATA RAMANA', 'Party': 'Andhra Rastra Praja Samithi', 'Status': 'Rejected', 
